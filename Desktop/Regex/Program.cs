a61c199897568a120127306da6fdbc84f6c7cdf8
Ashwini <salunkheashwini1998@gmail.com>
2023-04-02 16:49:39 +0530

    Method Calling Array List Crud Operation

---
+++ Desktop/Regex/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To Generic Concepts Problems");
+        Console.WriteLine("Select any one option \n 1. Delete Integer,double,character element in array");
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                int[] intArry = DeleteArrayElement.GetIntElement();
+                Console.WriteLine("Plese pick up any one element");
+                int delete = Convert.ToInt32(Console.ReadLine());
+                DeleteArrayElement.DeleteIntElement(intArry, delete);
+
+                double[] doubleArry = DeleteArrayElement.GetDoubleElement();
+                Console.WriteLine("Plese pick up any one element");
+                double doubleDelete = Convert.ToDouble(Console.ReadLine());
+                DeleteArrayElement.DeleteDoubleElement(doubleArry, doubleDelete);
+
+                char[] charArry = DeleteArrayElement.GetCharElement();
+                Console.WriteLine("Plese pick up any one element");
+                char charDelete = Convert.ToChar(Console.ReadLine());
+                DeleteArrayElement.DeleteCharElement(charArry, charDelete);
+                break;
+
+            default:
+                Console.WriteLine("Please Enter Right Option number From given List");
+                break;
+        }
+    }
+}
+}
---
+++ Desktop/add/ArrayListCRUD.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo3
+{
+    public class ArrayListCRUD
+    {
+        public static void ArraylistOperation()
+        {
+            // Create an ArrayList
+            ArrayList Car = new ArrayList();
+
+            // Add elements to the ArrayList
+            Console.WriteLine("Adding elements in Arraylist");
+            Car.Add("Hyundai");
+            Car.Add("BMW");
+            Car.Add("Maruti Suzuki");
+            foreach (string e in Car)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            // Read elements from the ArrayList
+            Console.WriteLine("------------------");
+            Console.WriteLine("Reading Elements from Arraylist");
+            Console.WriteLine("The first element is " + Car[0]);
+            Console.WriteLine("The second element is " + Car[1]);
+            Console.WriteLine("The third element is " + Car[2]);
+
+            // Update an element in the ArrayList
+            Car[1] = "Toyota";
+            Console.WriteLine("-----------------");
+            Console.WriteLine("After updating element, second car is now " + Car[1]);
+            foreach (string e in Car)
+            {
+                Console.WriteLine(e);
+            }
+
+            //Delete an element from the ArrayList
+            Car.Remove("Hyundai");
+            Console.WriteLine("------------------");
+            Console.WriteLine("After Deleting element, ArrayList now contains " + Car.Count + " elements");
+            foreach (string e in Car)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
+
